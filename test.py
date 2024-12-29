8c619a973bf1b4c5dd7a4a138f5c8f1911c224d0
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-12-29 22:37:40 +0000

    Refactor test script to aggregate experiment results.
    Replaced individual experiment runs with a call to `run_all_experiments` for better scalability and parallelization. Simplified print statements to output aggregated results directly.

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
 from train_dyna_q_parallel import *
 
-test_results, test_steps, final_test_reward = run_experiment("cartpole", 88)
-
-print(test_results)
-print(test_steps)
-print(final_test_reward)
+aggregated_results = run_all_experiments({"cartpole": 4}, max_workers=4)
+print(aggregated_results)
