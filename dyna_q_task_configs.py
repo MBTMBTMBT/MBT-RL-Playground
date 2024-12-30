2d8e9770230b24e251e0e9881281b43c7edb93df
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-12-30 00:18:55 +0000

    Increase parallel workers and update training schedules
    Increased `max_workers` and adjusted experiment parameters to enhance performance and scalability. Updated training step counts and policy schedules to refine exploration-exploitation balance and extend training durations for better model accuracy.

--- dyna_q_task_configs.py
+++ dyna_q_task_configs.py
@@ -33,7 +33,7 @@ def get_envs_discretizers_and_configs(name: str, configs_only=False):
             "exploit_policy_reward_rate": 1e-3,
             "exploit_value_decay": 0.99,
             "exploit_policy_training_per_num_steps": int(0.05e6),
-            "exploit_policy_training_steps": int(0.05e6),
+            "exploit_policy_training_steps": int(0.1e6),
             "exploit_policy_test_per_num_steps": int(0.05e6),
             "exploit_policy_test_episodes": 64,
             "save_per_num_steps": int(0.25e6),
@@ -53,7 +53,7 @@ def get_envs_discretizers_and_configs(name: str, configs_only=False):
                 "quality_value_threshold": 1.0,
                 "take_done_states_as_targets": True,
             },
-            int(0.1e6): {
+            int(0.25e6): {
                 "explore_policy_exploit_policy_ratio": (1.0, 0.0),
                 "train_exploit_policy": True,
                 "epsilon": 0.3,
@@ -62,7 +62,7 @@ def get_envs_discretizers_and_configs(name: str, configs_only=False):
                 "test_exploit_policy": True,
                 "test_exploit_strategy": "greedy",
             },
-            int(0.2e6): {
+            int(0.5e6): {
                 "explore_policy_exploit_policy_ratio": (0.5, 0.5),
                 "train_exploit_policy": True,
                 "epsilon": 0.2,
@@ -71,7 +71,7 @@ def get_envs_discretizers_and_configs(name: str, configs_only=False):
                 "test_exploit_policy": True,
                 "test_exploit_strategy": "greedy",
             },
-            int(0.3e6): {
+            int(1.0e6): {
                 "explore_policy_exploit_policy_ratio": (0.25, 0.75),
                 "train_exploit_policy": True,
                 "epsilon": 0.1,
