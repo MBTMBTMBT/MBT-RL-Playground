48e640efed9125ec93d1ce4c900fdd1d79f31853
Benteng Ma <benteng.ma@kcl.ac.uk>
2025-01-01 22:06:24 +0000

    Update experiment order in test script
    Rearranged the "cartpole" experiment to run earlier in the sequence. This improves the logical flow and organization of the script for better readability and execution. No functionality changes were introduced.

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ from train_dyna_q_parallel import *
 
 aggregated_results = run_all_experiments_and_plot({"texi": 4}, max_workers=16)
 print(aggregated_results)
+aggregated_results = run_all_experiments_and_plot({"cartpole": 4}, max_workers=8)
+print(aggregated_results)
 aggregated_results = run_all_experiments_and_plot({"mountain_car": 4}, max_workers=16)
 print(aggregated_results)
 aggregated_results = run_all_experiments_and_plot({"pendulum": 4}, max_workers=8)
-print(aggregated_results)
-aggregated_results = run_all_experiments_and_plot({"cartpole": 4}, max_workers=8)
 print(aggregated_results)
 aggregated_results = run_all_experiments_and_plot({"acrobot": 4}, max_workers=4)
 print(aggregated_results)
