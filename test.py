a9a00a8241a48eb0d6cd7abead41498c091e4b05
Benteng Ma <benteng.ma@kcl.ac.uk>
2025-01-01 22:04:59 +0000

    Adjust experiment parameters and error handling logic.
    Updated 'texi' experiment runs to use 4 workers instead of 8 and revised Q-Cut configuration to increase search space from 8 to 32. Simplified logic for error handling in target state search by reducing printed error count and streamlining decrement behavior. These changes enhance resource utilization and improve clarity in error reporting.

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from train_dyna_q_parallel import *
 
-aggregated_results = run_all_experiments_and_plot({"texi": 8}, max_workers=16)
+aggregated_results = run_all_experiments_and_plot({"texi": 4}, max_workers=16)
 print(aggregated_results)
 aggregated_results = run_all_experiments_and_plot({"mountain_car": 4}, max_workers=16)
 print(aggregated_results)
