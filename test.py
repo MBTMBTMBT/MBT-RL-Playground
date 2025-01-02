9f1bd35f20f80906940c68b60bc149a9e31ab9bd
Benteng Ma <benteng.ma@kcl.ac.uk>
2025-01-02 09:29:36 +0000

    Disable unused test experiments and update task config.
    Commented out unnecessary test scenarios in `test.py` to streamline the workflow. Adjusted test frequency and episode count in `dyna_q_task_configs.py` to enhance evaluation granularity.

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from train_dyna_q_parallel import *
 
-aggregated_results = run_all_experiments_and_plot({"texi": 4}, max_workers=16)
-print(aggregated_results)
-aggregated_results = run_all_experiments_and_plot({"cartpole": 4}, max_workers=8)
-print(aggregated_results)
+# aggregated_results = run_all_experiments_and_plot({"texi": 4}, max_workers=16)
+# print(aggregated_results)
+# aggregated_results = run_all_experiments_and_plot({"cartpole": 4}, max_workers=8)
+# print(aggregated_results)
 aggregated_results = run_all_experiments_and_plot({"mountain_car": 4}, max_workers=16)
 print(aggregated_results)
 aggregated_results = run_all_experiments_and_plot({"pendulum": 4}, max_workers=8)
