330c504d8cf82e9be2a51bbe19becc03b9735caa
Benteng Ma <benteng.ma@kcl.ac.uk>
2025-01-04 01:11:18 +0000

    Refactor test.py to add main guard and tweak parameters.
    Wrapped the script with `if __name__ == '__main__'` for better modularity and safe imports. Adjusted "texi" experiment parameters to use fewer workers, improving scalability and resource management.

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-from train_dyna_q_parallel_modernized import *
-
-aggregated_results = run_all_experiments_and_plot({"texi": 8}, max_workers=16)
-print(aggregated_results)
-aggregated_results = run_all_experiments_and_plot({"cartpole": 8}, max_workers=16)
-print(aggregated_results)
-aggregated_results = run_all_experiments_and_plot({"mountain_car": 8}, max_workers=16)
-print(aggregated_results)
-aggregated_results = run_all_experiments_and_plot({"pendulum": 8}, max_workers=16)
-print(aggregated_results)
-aggregated_results = run_all_experiments_and_plot({"acrobot": 4}, max_workers=8)
-print(aggregated_results)
+if __name__ == '__main__':
+    from train_dyna_q_parallel_modernized import *
+    aggregated_results = run_all_experiments_and_plot({"texi": 2}, max_workers=4)
+    print(aggregated_results)
+    aggregated_results = run_all_experiments_and_plot({"cartpole": 8}, max_workers=16)
+    print(aggregated_results)
+    aggregated_results = run_all_experiments_and_plot({"mountain_car": 8}, max_workers=16)
+    print(aggregated_results)
+    aggregated_results = run_all_experiments_and_plot({"pendulum": 8}, max_workers=16)
+    print(aggregated_results)
+    aggregated_results = run_all_experiments_and_plot({"acrobot": 4}, max_workers=8)
+    print(aggregated_results)
