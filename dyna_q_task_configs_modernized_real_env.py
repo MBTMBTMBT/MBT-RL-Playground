af137ce04a4b4f0d354369d113a68521936f9573
Benteng Ma <benteng.ma@kcl.ac.uk>
2025-01-13 20:41:32 +0000

    Update experiment scenarios and Dyna-Q iteration thresholds
    Refined the experiments in `test_deep.py` by altering environments and parameters. Adjusted iteration thresholds in `dyna_q_task_configs_modernized_real_env.py` to enable finer-grained control of training phases, improving balance between exploration and exploitation.

--- dyna_q_task_configs_modernized_real_env.py
+++ dyna_q_task_configs_modernized_real_env.py
@@ -245,28 +245,28 @@ def get_envs_discretizers_and_configs(name: str, configs_only=False):
                 "quality_value_threshold": 1.0,
                 "take_done_states_as_targets": False,
             },
-            int(100e3): {
+            int(50e3): {
                 "train_from_real_environment": False,
                 "explore_policy_exploit_policy_ratio": (0.75, 0.25),
                 "train_exploit_policy": True,
                 "test_exploit_policy": True,
                 "test_exploit_strategy": "greedy",
             },
-            int(200e3): {
+            int(100e3): {
                 "train_from_real_environment": False,
                 "explore_policy_exploit_policy_ratio": (0.5, 0.5),
                 "train_exploit_policy": True,
                 "test_exploit_policy": True,
                 "test_exploit_strategy": "greedy",
             },
-            int(300e3): {
+            int(200e3): {
                 "train_from_real_environment": False,
                 "explore_policy_exploit_policy_ratio": (0.25, 0.75),
                 "train_exploit_policy": True,
                 "test_exploit_policy": True,
                 "test_exploit_strategy": "greedy",
             },
-            int(500e3): {
+            int(250e3): {
                 "train_from_real_environment": True,
                 "train_exploit_policy": True,
                 "test_exploit_policy": True,
